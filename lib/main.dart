87745ebf8559bb20019338323fe3fa000e8a3e12
Jtility <leejewon97@gmail.com>
2024-05-31 00:44:06 +0900

    user-agent 적용 header를 통한 user-agent가 앱에선 필수, 웹에선 필수로 없어야함 때문에 HttpOverrides.global에 값을 넘겨줌으로서 해결

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,19 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:webtoon/screens/home_screen.dart';
 
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..userAgent =
+          'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36';
+  }
+}
+
 void main() {
+  HttpOverrides.global = MyHttpOverrides();
   runApp(const MyApp());
 }
 
