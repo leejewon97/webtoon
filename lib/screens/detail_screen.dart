4aa8b990307e5db6b4a44df89dbaa6f4787d482b
Jtility <leejewon97@gmail.com>
2024-06-05 18:37:52 +0900

    GestureDetector를 활용한 Navigator 적용. 화면 전환 구현 webtoon_widget.dart   기존 Column을 GestureDetector로 감쌈     onTap 이벤트 발생 시 Navigator.push()를 통해 DetailScreen으로 이동 lib/screens/detail_screen.dart   webtoon widget을 클릭했을 때 이동하는 화면   기존 home_screen에서 사용했던 Scaffold 및 AppBar 복사   단 Text는 webtoon의 title로 설정
    간단한 애니메이션 및 화면 전환(뒤로가기) 구현

---
+++ lib/screens/detail_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:webtoon/models/webtoon_model.dart';
+
+class DetailScreen extends StatelessWidget {
+  final WebtoonModel webtoon;
+
+  const DetailScreen({
+    super.key,
+    required this.webtoon,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          webtoon.title,
+          style: const TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        elevation: 3,
+        shadowColor: Colors.black,
+        backgroundColor: Colors.greenAccent[400],
+        foregroundColor: Colors.white,
+      ),
+    );
+  }
+}
--- lib/widgets/webtoon_widget.dart
+++ lib/widgets/webtoon_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:webtoon/models/webtoon_model.dart';
+import 'package:webtoon/screens/detail_screen.dart';
 
 class Webtoon extends StatelessWidget {
   final WebtoonModel webtoon;
@@ -11,29 +12,39 @@ class Webtoon extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          width: 200,
-          clipBehavior: Clip.hardEdge,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black.withOpacity(0.75),
-                blurRadius: 10,
-                offset: const Offset(5, 5),
-              ),
-            ],
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => DetailScreen(webtoon: webtoon),
           ),
-          child: Image.network(webtoon.thumb),
-        ),
-        const SizedBox(height: 20),
-        Text(
-          webtoon.title,
-          style: const TextStyle(fontSize: 16),
-        ),
-      ],
+        );
+      },
+      child: Column(
+        children: [
+          Container(
+            width: 200,
+            clipBehavior: Clip.hardEdge,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.75),
+                  blurRadius: 10,
+                  offset: const Offset(5, 5),
+                ),
+              ],
+            ),
+            child: Image.network(webtoon.thumb),
+          ),
+          const SizedBox(height: 20),
+          Text(
+            webtoon.title,
+            style: const TextStyle(fontSize: 16),
+          ),
+        ],
+      ),
     );
   }
 }
