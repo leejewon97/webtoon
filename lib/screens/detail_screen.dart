ebf57ababb358280ef8435d738ff26a5f19f7fb8
Jtility <leejewon97@gmail.com>
2024-06-21 19:56:55 +0900

    [완성] 앱 내 데이터 저장 방식으로 웹툰별 좋아요 구현 detail_screen.dart   SharedPreferences에 bool타입으로 웹툰id를 키, isLiked를 값으로 저장   initState() : initPrefs() 호출   initPrefs()     prefs를 초기화하고, 현재 웹툰의 isLiked가 저장되어 있는 지 확인       저장되어 있지 않다면 저장하고, isLiked를 prefs에서 가져와서 상태에 반영   onLikePressed() : isLiked를 반전시키고, prefs에 반영

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:webtoon/models/webtoon_detail_model.dart';
 import 'package:webtoon/models/webtoon_episode_model.dart';
 import 'package:webtoon/models/webtoon_model.dart';
@@ -20,12 +21,32 @@ class DetailScreen extends StatefulWidget {
 class _DetailScreenState extends State<DetailScreen> {
   late Future<WebtoonDetailModel> webtoonDetail;
   late Future<List<WebtoonEpisodeModel>> episodes;
+  late SharedPreferences prefs;
+  bool isLiked = false;
+
+  initPrefs() async {
+    prefs = await SharedPreferences.getInstance();
+    if (!prefs.containsKey(widget.webtoon.id)) {
+      await prefs.setBool(widget.webtoon.id, isLiked);
+    }
+    setState(() {
+      isLiked = prefs.getBool(widget.webtoon.id)!;
+    });
+  }
 
   @override
   void initState() {
     super.initState();
     webtoonDetail = ApiService.getWebtoonDetail(widget.webtoon.id);
     episodes = ApiService.getLatestWebtoonEpisodes(widget.webtoon.id);
+    initPrefs();
+  }
+
+  onLikePressed() async {
+    setState(() {
+      isLiked = !isLiked;
+    });
+    await prefs.setBool(widget.webtoon.id, isLiked);
   }
 
   @override
@@ -41,6 +62,14 @@ class _DetailScreenState extends State<DetailScreen> {
             fontWeight: FontWeight.bold,
           ),
         ),
+        actions: [
+          IconButton(
+            icon: isLiked
+                ? const Icon(Icons.favorite_outlined)
+                : const Icon(Icons.favorite_outline_sharp),
+            onPressed: onLikePressed,
+          ),
+        ],
         elevation: 3,
         shadowColor: Colors.black,
         backgroundColor: Colors.greenAccent[400],
